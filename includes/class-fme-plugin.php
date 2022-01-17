d0a41868a4171a6e0ed69834c60b130313069dfa
Eduardo Villão <eduardovillao@MacBook-Pro-de-Eduardo.local>
2022-01-17 10:57:32 -0300

    singleton improvements

--- includes/class-fme-plugin.php
+++ includes/class-fme-plugin.php
@@ -48,6 +48,31 @@ final class FME_Plugin {
 	}
 
 	/**
+	 * Disable class cloning and throw an error on object clone.
+	 *
+	 * The whole idea of the singleton design pattern is that there is a single
+	 * object. Therefore, we don't want the object to be cloned.
+	 *
+	 * @access public
+	 * @since 1.6
+	 */
+	public function __clone() {
+		// Cloning instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, esc_html__( 'Something went wrong.', 'form-masks-for-elementor' ), '1.6' );
+	}
+
+	/**
+	 * Disable unserializing of the class.
+	 *
+	 * @access public
+	 * @since 1.6
+	 */
+	public function __wakeup() {
+		// Unserializing instances of the class is forbidden.
+		_doing_it_wrong( __FUNCTION__, esc_html__( 'Something went wrong.', 'form-masks-for-elementor' ), '1.6' );
+	}
+
+	/**
 	 * Constructor
 	 *
 	 * Private method for prevent instance outsite the class.
