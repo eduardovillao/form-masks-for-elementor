25f60d1db593c75f545be2bdcb9ab8bc99a4f4b8
Eduardo <dev@eduardovillao.me>
2024-04-06 22:46:01 -0300

    support to wp 6.5

--- form-masks-for-elementor.php
+++ form-masks-for-elementor.php
@@ -5,8 +5,8 @@
  * Description: Form Masks for Elementor create a custom control in field advanced tab for your customize your fields with masks. This plugin require the Elementor Pro (Form Widget).
  * Author: EduardoVillao.me
  * Author URI: https://eduardovillao.me/
- * Version: 1.6.3
- * Requires at least: 5.4
+ * Version: 1.6.4
+ * Requires at least: 5.5
  * Requires PHP: 7.0
  * Text Domain: form-masks-for-elementor
  * License: GPL-2.0+
@@ -29,14 +29,14 @@ along with Form Masks for Elementor. If not, see http://www.gnu.org/licenses/gpl
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 define( 'FME_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FME_PLUGN_URL', plugin_dir_url( __FILE__ ) );
-define( 'FME_VERSION' , '1.6.3' );
+define( 'FME_VERSION' , '1.6.4' );
 define( 'FME_PHP_MINIMUM_VERSION', '7.0' );
-define( 'FME_WP_MINIMUM_VERSION', '5.4' );
+define( 'FME_WP_MINIMUM_VERSION', '5.5' );
 
 /**
  * Check PHP and WP version before include plugin class
@@ -44,15 +44,12 @@ define( 'FME_WP_MINIMUM_VERSION', '5.4' );
  * @since 1.6
  */
 if( ! version_compare( PHP_VERSION, FME_PHP_MINIMUM_VERSION, '>=' ) ) {
-
 	add_action( 'admin_notices', 'fme_admin_notice_php_version_fail' );
 
 } elseif( ! version_compare( get_bloginfo( 'version' ), FME_WP_MINIMUM_VERSION, '>=' ) ) {
-
 	add_action( 'admin_notices', 'fme_admin_notice_wp_version_fail' );
 
 } else {
-
 	include_once FME_PLUGIN_PATH . 'includes/class-fme-plugin.php';
 }
 
@@ -63,7 +60,6 @@ if( ! version_compare( PHP_VERSION, FME_PHP_MINIMUM_VERSION, '>=' ) ) {
  * @return void
  */
 function fme_admin_notice_php_version_fail() {
-
 	$message = sprintf(
 		esc_html__( '%1$s requires PHP version %2$s or greater.', 'form-masks-for-elementor' ),
 		'<strong>Form masks for Elementor</strong>',
@@ -71,7 +67,6 @@ function fme_admin_notice_php_version_fail() {
 	);
 
 	$html_message = sprintf( '<div class="notice notice-error"><p>%1$s</p></div>', $message );
-
 	echo wp_kses_post( $html_message );
 }
 
@@ -82,7 +77,6 @@ function fme_admin_notice_php_version_fail() {
  * @return void
  */
 function fme_admin_notice_wp_version_fail() {
-
 	$message = sprintf(
 		esc_html__( '%1$s requires WordPress version %2$s or greater.', 'form-masks-for-elementor' ),
 		'<strong>Form masks for Elementor</strong>',
@@ -90,6 +84,5 @@ function fme_admin_notice_wp_version_fail() {
 	);
 
 	$html_message = sprintf( '<div class="notice notice-error"><p>%1$s</p></div>', $message );
-
 	echo wp_kses_post( $html_message );
 }
